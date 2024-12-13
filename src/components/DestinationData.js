ee0a9c2660901488d71a3f80ecba49fe5573ad9c
Felix <felix.mathiasson99@gmail.com>
2024-12-13 14:35:28 +0100

    vacation images added, project done

--- src/components/DestinationData.js
+++ src/components/DestinationData.js
@@ -29,7 +29,68 @@ const destinations = [
       image: "/assets/barcelona.jpg",
       desc: "A vibrant city in Spain known for Gaudí's architectural masterpieces, lively streets, and Mediterranean beaches.",
     },
-  ];
+    {
+      name: "Paris",
+      image: "/assets/paris.jpg",
+      desc: "The City of Light, famous for its romantic ambiance, iconic landmarks like the Eiffel Tower, and its rich cultural heritage.",
+    },
+    {
+      name: "Rome",
+      image: "/assets/rome.jpg",
+      desc: "A city rich in history and ancient architecture, home to the Colosseum, Vatican City, and a wealth of cultural experiences.",
+    },
+    {
+      name: "Sydney",
+      image: "/assets/sydney.jpg",
+      desc: "A vibrant city in Australia, known for the Sydney Opera House, stunning beaches, and a laid-back lifestyle.",
+    },
+    {
+      name: "Dubai",
+      image: "/assets/dubai.jpg",
+      desc: "A futuristic city in the UAE, known for its luxury shopping, towering skyscrapers, and extravagant lifestyle.",
+    },
+    {
+      name: "Cairo",
+      image: "/assets/cairo.jpg",
+      desc: "A bustling city with a mix of ancient wonders and modern life, home to the Pyramids of Giza and the Sphinx.",
+    },
+    {
+      name: "Cape Town",
+      image: "/assets/capeTown.jpg",
+      desc: "A coastal city in South Africa, known for its stunning landscapes, including Table Mountain and pristine beaches.",
+    },
+    {
+      name: "Rio de Janeiro",
+      image: "/assets/rioDeJaneiro.jpg",
+      desc: "A vibrant Brazilian city, famous for its lively carnival, stunning beaches, and the Christ the Redeemer statue.",
+    },
+    {
+      name: "London",
+      image: "/assets/london.jpg",
+      desc: "The capital of the UK, known for its royal landmarks, historic monuments, and diverse cultural scene.",
+    },
+    {
+      name: "Amsterdam",
+      image: "/assets/amsterdam.jpg",
+      desc: "A charming Dutch city, famous for its canals, bike culture, and art museums like the Van Gogh Museum.",
+    },
+    {
+      name: "Hawaii",
+      image: "/assets/hawaii.jpg",
+      desc: "A tropical paradise in the United States, known for its stunning beaches, volcanoes, and vibrant Polynesian culture.",
+    },
+    {
+      name: "Santorini",
+      image: "/assets/santorini.jpg",
+      desc: "A picturesque Greek island, famous for its white-washed buildings, blue-domed churches, and breathtaking sunsets.",
+    },
+    {
+      name: "Bali",
+      image: "/assets/bali.jpg",
+      desc: "A tropical island in Indonesia, known for its lush landscapes, beautiful beaches, and vibrant culture.",
+    },
+];
+
   
   export default destinations;
   
